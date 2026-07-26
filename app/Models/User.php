3e4c91db5a75886f9a4a4858c6d03c98dd375519
DedenKoesuma <dedenkoesuma72@gmail.com>
2026-07-26 17:32:44 +0700

    feat:add feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models;
-
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
